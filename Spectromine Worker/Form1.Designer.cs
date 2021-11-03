62b3d5275fa774eb5021207a149ca9153bb27768
Xiaofeng Xie <77813931+xiaofengxie128@users.noreply.github.com>
2021-11-02 19:03:01 -0600

    fix server down and network down issue

--- Spectromine Worker/Form1.Designer.cs
+++ Spectromine Worker/Form1.Designer.cs
@@ -29,33 +29,46 @@ namespace Spectromine_Worker
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.hostip = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.workpath = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.workername = new System.Windows.Forms.TextBox();
+            this.workerstatus = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lastupdate = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.currentjob = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.workerip = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // output
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(447, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(341, 426);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.output.BackColor = System.Drawing.SystemColors.Info;
+            this.output.Location = new System.Drawing.Point(447, 12);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.output.Size = new System.Drawing.Size(341, 426);
+            this.output.TabIndex = 0;
+            this.output.Text = "";
             // 
-            // textBox1
+            // hostip
             // 
-            this.textBox1.Location = new System.Drawing.Point(125, 26);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.hostip.Location = new System.Drawing.Point(125, 26);
+            this.hostip.Name = "hostip";
+            this.hostip.Size = new System.Drawing.Size(226, 20);
+            this.hostip.TabIndex = 1;
             // 
             // label1
             // 
@@ -70,18 +83,18 @@ namespace Spectromine_Worker
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Location = new System.Drawing.Point(12, 59);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Worker Name:";
             // 
-            // textBox2
+            // workpath
             // 
-            this.textBox2.Location = new System.Drawing.Point(140, 120);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.workpath.Location = new System.Drawing.Point(15, 149);
+            this.workpath.Name = "workpath";
+            this.workpath.Size = new System.Drawing.Size(426, 20);
+            this.workpath.TabIndex = 3;
             // 
             // label3
             // 
@@ -94,55 +107,151 @@ namespace Spectromine_Worker
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(246, 117);
+            this.button1.Location = new System.Drawing.Point(140, 120);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
             this.button1.Text = "Select";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // textBox3
+            // workername
             // 
-            this.textBox3.Location = new System.Drawing.Point(125, 73);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 1;
+            this.workername.Location = new System.Drawing.Point(125, 57);
+            this.workername.Name = "workername";
+            this.workername.Size = new System.Drawing.Size(226, 20);
+            this.workername.TabIndex = 1;
             // 
-            // textBox4
+            // workerstatus
             // 
-            this.textBox4.Location = new System.Drawing.Point(125, 187);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 1;
+            this.workerstatus.Location = new System.Drawing.Point(115, 184);
+            this.workerstatus.Name = "workerstatus";
+            this.workerstatus.Size = new System.Drawing.Size(236, 20);
+            this.workerstatus.TabIndex = 1;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 190);
+            this.label4.Location = new System.Drawing.Point(12, 187);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(76, 13);
             this.label4.TabIndex = 4;
-            this.label4.Text = "Worker Name:";
+            this.label4.Text = "Worker status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Task Percentage:";
+            // 
+            // lastupdate
+            // 
+            this.lastupdate.Location = new System.Drawing.Point(115, 269);
+            this.lastupdate.Name = "lastupdate";
+            this.lastupdate.Size = new System.Drawing.Size(236, 20);
+            this.lastupdate.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 272);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Last update:";
+            // 
+            // currentjob
+            // 
+            this.currentjob.Location = new System.Drawing.Point(97, 313);
+            this.currentjob.Name = "currentjob";
+            this.currentjob.Size = new System.Drawing.Size(294, 20);
+            this.currentjob.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 313);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Current Job:";
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(25, 359);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(109, 48);
+            this.Start.TabIndex = 7;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // workerip
+            // 
+            this.workerip.Location = new System.Drawing.Point(125, 83);
+            this.workerip.Name = "workerip";
+            this.workerip.Size = new System.Drawing.Size(226, 20);
+            this.workerip.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Worker ip:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(242, 359);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(109, 48);
+            this.stop.TabIndex = 8;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(116, 225);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(226, 23);
+            this.progressBar1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.Start);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.workpath);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.currentjob);
+            this.Controls.Add(this.lastupdate);
+            this.Controls.Add(this.workerstatus);
+            this.Controls.Add(this.workerip);
+            this.Controls.Add(this.workername);
+            this.Controls.Add(this.hostip);
+            this.Controls.Add(this.output);
             this.Name = "Form1";
             this.Text = "Spectromine Worker";
             this.ResumeLayout(false);
@@ -152,17 +261,27 @@ namespace Spectromine_Worker
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox output;
+        private System.Windows.Forms.TextBox hostip;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox workpath;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox workername;
+        private System.Windows.Forms.TextBox workerstatus;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox lastupdate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox currentjob;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox workerip;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- Spectromine Worker/Form1.cs
+++ Spectromine Worker/Form1.cs
@@ -5,16 +5,30 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Spectromine_Worker
 {
+
     public partial class Form1 : Form
     {
+        BackgroundWorker backgroundWorker1 = new BackgroundWorker();
+
         public Form1()
         {
             InitializeComponent();
+            workerip.Text = GetIPAddress();
+            workername.Text = "worker_" + Gethostname();
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;  //Tell the user how the process went
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true; //Allow for the process to be cancelled
+
         }
 
         private void eventLog1_EntryWritten(object sender, System.Diagnostics.EntryWrittenEventArgs e)
@@ -26,5 +40,117 @@ namespace Spectromine_Worker
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog
+                {
+                    Title = "Browse Spectromine.exe Files",
+
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+                    DefaultExt = "exe",
+                    Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*",
+                    RestoreDirectory = true,
+
+                };
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    workpath.Text = openFileDialog1.FileName;
+                }
+            }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private string GetIPAddress()
+        {
+            StringBuilder sb = new StringBuilder();
+            String strHostName = string.Empty;
+            strHostName = Dns.GetHostName();
+            IPHostEntry ipHostEntry = Dns.GetHostEntry(strHostName);
+            IPAddress[] address = ipHostEntry.AddressList;
+            sb.Append(address[4].ToString());
+            sb.AppendLine();
+            return sb.ToString();
+        }
+
+        private string Gethostname()
+        {
+            StringBuilder sb = new StringBuilder();
+            String strHostName = string.Empty;
+            strHostName = Dns.GetHostName();
+            return strHostName.ToString();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+            output.AppendText(Environment.NewLine + "Worker started");
+        }
+
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //stop the thread.
+            //Check if background worker is doing anything and send a cancellation if it is
+            if (backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.CancelAsync();
+            }
+            output.AppendText(Environment.NewLine + "Worker stopped");
+
+        }
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                backgroundWorker1.ReportProgress(i);
+
+                //Check if there is a request to cancel the process
+                if (backgroundWorker1.CancellationPending)
+                {
+                    e.Cancel = true;
+                    backgroundWorker1.ReportProgress(0);
+                    return;
+                }
+            }
+            //If the process exits the loop, ensure that progress is set to 100%
+            //Remember in the loop we set i < 100 so in theory the process will complete at 99%
+            backgroundWorker1.ReportProgress(100);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                output.AppendText(Environment.NewLine + "Process was cancelled");
+            }
+            else if (e.Error != null)
+            {
+                output.AppendText(Environment.NewLine + "There was an error running the process. The thread aborted");
+            }
+            else
+            {
+                output.AppendText(Environment.NewLine + "Process was completed");
+            }
+        }
+
+
+
     }
 }
